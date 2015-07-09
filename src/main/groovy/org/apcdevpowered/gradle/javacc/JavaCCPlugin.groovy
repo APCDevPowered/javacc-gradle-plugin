604b0fce229f999536d90c8ce2fb57b265641b27
yuxuanchiadm <yuxuanchiadm@126.com>
2015-07-10 06:25:35 +0800

    Add project code

---
+++ src/main/groovy/org/apcdevpowered/gradle/javacc/JavaCCPlugin.groovy
@@ -0,0 +1,10 @@
+package org.apcdevpowered.gradle.javacc
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project;
+
+class JavaCCPlugin implements Plugin<Project> {
+    @Override
+    public void apply(Project project) {
+    }
+}
